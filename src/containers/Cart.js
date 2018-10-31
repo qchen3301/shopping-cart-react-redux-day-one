d1ec5a2a5069ce3e5b4c4f1067e514c7ad385b0b
qchen3301 <42414585+qchen3301@users.noreply.github.com>
2018-10-31 17:03:43 -0400

    cart.js container

---
+++ src/containers/Cart.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import { bindAdctionCreators, bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+import * as CartActions from '../actions/CartActions.js'
+import Shelf from '../components/Shelf'
+
+class Cart extends Component {
+    state = {}
+  render() {
+      const CartItems = this.props.cart.map( (item, id) => {
+          return (
+              <li key={id}>
+                {item}
+              </li>
+          )
+      })
+    return (
+      <div>
+        <Shelf addItem={this.props.actions.addToCart} />
+        <h2>Cart</h2>
+        <ol>
+            {CartItems}
+        </ol>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state, props) => {
+    return {
+        cart: state.cart
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        actions: bindActionCreators(CartACtions, dispatch)
+    }
+}
+
+const wrapperFunction = connect(mapStateToProps, mapDispatchToProps)
+
+const wrappedComponent = wrapperFunction(cart)
+
+export default wrappedComponent
