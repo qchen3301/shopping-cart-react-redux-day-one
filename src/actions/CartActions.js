a583dba43f716cd3828ceab33624f2c8a48b22bd
qchen3301 <42414585+qchen3301@users.noreply.github.com>
2018-10-31 16:25:37 -0400

    cartactions.js

--- src/actions/CartActions.js
+++ src/actions/CartActions.js
@@ -1,5 +1,14 @@
 //Action Creator function
 //This action will be called from a button in the UI
 export const addToCart = (item) => {
+    //This console.log is a side effect and technically makes this function 'impure'
+    //It may come in handy when testing to see if we've intergrated redux successfully
+    //to determine if our actions are firing
 
+    console.log(`ACTION: adding ${item} to cart`)
+    return {
+        //actions must have a type property
+        type: 'ADD_ITEM', //action naming convetions: all caps with snake-case (JAVASCRIPT_CONSTANT naming convention)
+        item //ESG shorthand again {item: item}
+    }
 }
